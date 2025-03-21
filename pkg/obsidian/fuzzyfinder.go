c4abb439bccad12249065b00063ad344685ec6f8
Drew Colthorp <colthorp@atomicobject.com>
2025-03-21 08:28:16 -0400

    Add FuzzyMatch function for simple pattern matching

--- pkg/obsidian/fuzzyfinder.go
+++ pkg/obsidian/fuzzyfinder.go
@@ -2,6 +2,7 @@ package obsidian
 
 import (
 	"errors"
+	"strings"
 	"github.com/ktr0731/go-fuzzyfinder"
 )
 
@@ -24,4 +25,20 @@ func (f *FuzzyFinder) Find(slice interface{}, itemFunc func(i int) string, opts 
 		return -1, errors.New(NoteDoesNotExistError)
 	}
 	return index, nil
+}
+
+// FuzzyMatch performs simplified fuzzy matching for file paths
+// Returns true if pattern is found within the file path (case insensitive)
+func FuzzyMatch(pattern, path string) bool {
+	if pattern == "" {
+		return false
+	}
+	
+	// Case insensitive matching
+	lowerPattern := strings.ToLower(pattern)
+	lowerPath := strings.ToLower(path)
+	
+	// Simple contains check - a more sophisticated algorithm could be used 
+	// for better fuzzy matching, but this works for basic search needs
+	return strings.Contains(lowerPath, lowerPattern)
 }
